88decd0b7f6ccf102b5e66227b144e547bdfe3bf
Soad Ehab <soadehab510@gmail.com>
2023-04-19 11:23:00 +0200

    add service

---
+++ Server/src/main/java/gov/iti/jets/web/models/CityModel.java
@@ -0,0 +1,50 @@
+package gov.iti.jets.web.models;
+
+import gov.iti.jets.web.entities.Address;
+import gov.iti.jets.web.entities.Country;
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.List;
+
+public class CityModel {
+
+    private Short cityId;
+
+    private String city;
+
+    private Date lastUpdate;
+    private Country countryId;
+
+    public Short getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Short cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Country getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Country countryId) {
+        this.countryId = countryId;
+    }
+}
